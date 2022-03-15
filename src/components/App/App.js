62ce90fcc51f9d58484d209a02cba0b93200ee3b
anandbaraik <anandbaraik2014@gmail.com>
2022-03-15 23:00:10 +0530

    feat:timer implemented :sparkles:

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,59 +3,76 @@ import "./App.css";
 import Nav from "../Nav/Nav";
 import Landing from "../Landing/Landing";
 import Footer from "../Footer/Footer";
-import ChallengeSection from '../ChallengeSection/ChallengeSection';
+import ChallengeSection from "../ChallengeSection/ChallengeSection";
 const __totalTime = 60;
 const serviceUrl = "http://metaphorpsum.com/paragraphs/1/9";
-class App extends React.Component{
-     state = {
-        selectedParagraph : "Hello World",
-        timerStarted : false,
-        timeRemaining : __totalTime,
-        words : 0,
-        charecters : 0,
-        wpm : 0,
-        testInfo : []
-    }
+class App extends React.Component {
+  state = {
+    timerStarted: false,
+    timeRemaining: __totalTime,
+    words: 0,
+    charecters: 0,
+    wpm: 0,
+    testInfo: [],
+  };
 
-    componentDidMount() {
-        fetch(serviceUrl)
-            .then((response) => response.text())
-            .then((data) => {
-                const selectedParagraphArray = data.split("");
-                const testInfo = selectedParagraphArray.map(selectedLetter => {
-                    return {
-                        testLetter : selectedLetter,
-                        status : "notAttempted"
-                    }
-                });
-                this.setState({testInfo});
-            });
-    }
+  componentDidMount() {
+    fetch(serviceUrl)
+      .then((response) => response.text())
+      .then((data) => {
+        const selectedParagraphArray = data.split("");
+        const testInfo = selectedParagraphArray.map((selectedLetter) => {
+          return {
+            testLetter: selectedLetter,
+            status: "notAttempted",
+          };
+        });
+        this.setState({ testInfo });
+      });
+  }
 
-    render(){
-        return(
-            <div className="app">
-                {/* Nav section */}
-                <Nav />
+  startTimer = () => {
+    this.setState({timerStarted:true});
+    
+      const timer = setInterval(() => {
+        if(this.state.timeRemaining > 0) {
+          this.setState({
+            timeRemaining: this.state.timeRemaining - 1
+          })
+        } else {
+          clearInterval(timer);
+        }
+      }, 1000);
+  }
 
-                {/* Landing page */}
-                <Landing/>
+  handleInput = (inputValue) => {
+    if(!this.state.timerStarted) this.startTimer();
+    console.log(inputValue);
+  }
+  render() {
+    return (
+      <div className="app">
+        {/* Nav section */}
+        <Nav />
 
-                {/* Challenege section */}
-                <ChallengeSection 
-                    selectedParagraph={this.state.selectedParagraph}
-                    timerStarted={this.state.timerStarted}
-                    timeRemaining={this.state.timeRemaining}
-                    words={this.state.words}
-                    charecters={this.state.charecters}
-                    wpm={this.state.wpm}
-                    testInfo={this.state.testInfo}
-                />
-                {/* Footer */}
-                <Footer/>
-            </div>
-        )
-    }
+        {/* Landing page */}
+        <Landing />
+
+        {/* Challenege section */}
+        <ChallengeSection
+          timerStarted={this.state.timerStarted}
+          timeRemaining={this.state.timeRemaining}
+          words={this.state.words}
+          charecters={this.state.charecters}
+          wpm={this.state.wpm}
+          testInfo={this.state.testInfo}
+          onInputChange={this.handleInput}
+        />
+        {/* Footer */}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
